b0035afcd0f1d1d1586dae667523f3ef7070bbbf
tonycastle <castleantony6@gmail.com>
2025-02-24 10:27:45 +0100

    added varaiants and button sizes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,14 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-//import { authHandler, initAuthConfig, verifyAuth } from "@hono/auth-js";
-//import Credentials from "next-auth/providers/credentials";
 import workspaces from "@/features/workspaces/server/route";
 import members from "@/features/members/server/route";
 import projects from "@/features/projects/server/route";
 import tasks from "@/features/tasks/server/route";
-//import { auth } from "@/auth";
 import { authMiddleware } from "@/features/auth/server/authMiddleware";
-//import { authUser } from "@/lib/dbService/users";
 
 //export const runtime = "edge";
 
@@ -16,6 +12,7 @@ const app = new Hono().basePath("/api");
 
 app.use("*", authMiddleware);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
   .route("/workspace", workspaces)
   .route("/members", members)
