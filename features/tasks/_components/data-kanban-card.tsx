1617745833122d3bc4feb5c963e406b3aa46f276
tonycastle <castleantony6@gmail.com>
2025-07-09 02:54:34 +0200

    fixed kanban colour issues

--- features/tasks/_components/data-kanban-card.tsx
+++ features/tasks/_components/data-kanban-card.tsx
@@ -13,11 +13,11 @@ interface DataKanbanCardProps {
 
 const DataKanbanCard = ({ task }: DataKanbanCardProps) => {
   return (
-    <div className="bg-white p-2.5 mb-1.5 rounded shadow-sm space-y-3">
+    <div className="p-2.5 mb-1.5 rounded shadow-sm space-y-3">
       <div className="flex items-start justify-between gap-x-2">
         <p className="text-sm line-clamp-2">{task.name}</p>
         <TaskActions id={task.id} projectId={task.projectId}>
-          <MoreHorizontal className="size-[18px] stroke-1 shrink-0  text-neutral-700 opacity-75 transition" />
+          <MoreHorizontal className="size-[18px] stroke-1 shrink-0  text-neutral-700 opacity-75 transition cursor-pointer" />
         </TaskActions>
       </div>
       <DottedSeparator />
