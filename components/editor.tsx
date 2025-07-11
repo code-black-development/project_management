0874e28927df726b307a4385485a8a1cb625fc0d
tonycastle <castleantony6@gmail.com>
2025-07-11 16:04:40 +0200

    dm editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
-import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
-import 'react-quill-new/dist/quill.snow.css';
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import "react-quill-new/dist/quill.snow.css";
 
 interface EditorProps {
-	value: string;
-	onChange: (value: string) => void;
+  value: string;
+  onChange: (value: string) => void;
 }
 
 export const Editor = ({ value, onChange }: EditorProps) => {
-	const ReactQuill = useMemo(
-		() => dynamic(() => import('react-quill-new'), { ssr: false }),
-		[]
-	);
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill-new"), { ssr: false }),
+    []
+  );
 
-	return (
-		<div className="bg-white dark:bg-gray-900">
-			<ReactQuill theme="snow" value={value} onChange={onChange} />
-		</div>
-	);
+  return (
+    <div className="bg-white dark:bg-gray-900">
+      <ReactQuill theme="snow" value={value} onChange={onChange} />
+    </div>
+  );
 };
