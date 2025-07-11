a6ecba19ee2d2c934753dddbe6956d30f10441ce
tonycastle <castleantony6@gmail.com>
2025-07-11 15:42:04 +0200

    fixed task description bug respects dark mode

--- components/preview.tsx
+++ components/preview.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
-import 'react-quill-new/dist/quill.bubble.css';
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import "react-quill-new/dist/quill.bubble.css";
 
 interface PreviewProps {
-	value: string;
+  value: string;
 }
 
 export const Preview = ({ value }: PreviewProps) => {
-	const ReactQuill = useMemo(
-		() => dynamic(() => import('react-quill-new'), { ssr: false }),
-		[]
-	);
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill-new"), { ssr: false }),
+    []
+  );
 
-	return (
-		<div>
-			<ReactQuill theme="bubble" value={value} readOnly />
-		</div>
-	);
+  return (
+    <div>
+      <ReactQuill theme="bubble" value={value} readOnly />
+    </div>
+  );
 };
